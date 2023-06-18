9edc2929e212c10543f09691d1d791acf35b2fd2
Kamalov Keldibek <klbest1797@gmail.com>
2023-06-19 00:43:35 +0300

    exercise 23

---
+++ Exercise_23/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+*/
+
+void Cube(int number)
+{
+    int current = 1;
+    while(current <= number)
+    {
+        double loky = Math.Pow(current, 3);
+        Console.Write($"{loky : 0}, ");
+        current++;
+    }
+    Console.WriteLine("\b\b.");
+}
+Console.WriteLine("number");
+int user_n = Convert.ToInt32(Console.ReadLine());
+Cube(user_n);
