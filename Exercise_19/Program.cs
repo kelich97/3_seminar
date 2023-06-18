0e01c1d91631a09cd02c3a59f6559a67ccf42360
Kamalov Keldibek <klbest1797@gmail.com>
2023-06-19 00:13:40 +0300

    exercise 19

---
+++ Exercise_19/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+void Nemo(int arg )
+{
+    int a = (number / 10000);
+    int b = (number / 1000 % 10);
+    int c = (number % 100 / 10);
+    int d = (number % 10);
+    if((a == d) && (b == c))
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+Nemo(number);
+
